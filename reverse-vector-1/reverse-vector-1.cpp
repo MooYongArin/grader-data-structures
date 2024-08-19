676eafbd3dddd07803659d54f8ac39d256f31ae7
MooYongArin <132063700+MooYongArin@users.noreply.github.com>
2024-08-19 10:55:16 +0700

    reverse-vector-1 and gitignore

---
+++ reverse-vector-1/reverse-vector-1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void reverse(vector<int> &v, int a, int b)
+{
+    // write your code only in this function
+    int count = 0;
+    std::vector<int> temp(v);
+    for (size_t i = a; i <= b; i++)
+    {
+        if (i <= (a+b)/2)
+        {
+            v[i] = temp[b-(i-a)];
+        }else {
+            v[i] = temp[b-count];
+        }
+        count++;
+    }
+    
+}
+int main()
+{
+    // read input
+    int n, a, b;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int c;
+        cin >> c;
+        v.push_back(c);
+    }
+    cin >> a >> b;
+    // call function
+    reverse(v, a, b);
+    // display content of the vector
+    // for (auto &x : v){
+    //     cout << x << " ";
+    //     }
+    for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    cout << endl;
+} // g++ reverse-vector-1.cpp -o reverse-vector-1
