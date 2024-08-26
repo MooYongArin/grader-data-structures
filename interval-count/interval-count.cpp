4deb6463362fe2285ff21d67261a61594e185bf5
MooYongArin <132063700+MooYongArin@users.noreply.github.com>
2024-08-26 22:04:26 +0700

    vector-to-map-interval-count

--- interval-count/interval-count.cpp
+++ interval-count/interval-count.cpp
@@ -2,7 +2,7 @@
 #include <map>
 #include <vector>
 #include <set>
-
+#include <algorithm>
 
 using namespace std;
 
@@ -10,15 +10,15 @@ int main()
 {
     std::ios_base::sync_with_stdio(false); std::cin.tie(0); 
     int n, m, k;
-    // vector<int> v;
-    set<int> s;
+    vector<int> v;
+    // set<int> s;
     cin >> n >> m >> k;
     for (size_t i = 0; i < n; i++)
     {
         int a;
         cin >> a;
-        // v.push_back(a);
-        s.insert(a);
+        v.push_back(a);
+        // s.insert(a);
     } 
     for (size_t i = 0; i < m; i++)
     {
@@ -41,8 +41,9 @@ int main()
         //     }
         // }
 
-        auto start = s.lower_bound(p - k);
-        auto end = s.upper_bound(p + k);
+        sort(v.begin(), v.end());
+        auto start = lower_bound(v.begin(), v.end(), p - k);
+        auto end = upper_bound(v.begin(), v.end(), p + k);
         count = distance(start, end);
 
         cout << count << " ";
