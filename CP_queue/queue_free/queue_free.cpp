d6bb4fe1bfdb8a4ccafaa50e2f28dfafc1fe705b
MooYongArin <yongariyata@gmail.com>
2024-10-01 12:04:18 +0700

    queue_free

---
+++ CP_queue/queue_free/queue_free.cpp
@@ -0,0 +1,32 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    // Write C++ code here
+    int n, mCap, mSize;
+    mCap = 1;
+    mSize = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int a, k;
+        cin >> a >> k;
+        if (a == 0)
+        {
+            mSize += k;
+            while (mSize > mCap)
+            {
+                mCap *= 2;
+            }
+        }
+        if (a == 1)
+        {
+            mSize -= k;
+        }
+    }
+    cout << mCap - mSize;
+
+    return 0;
+}
