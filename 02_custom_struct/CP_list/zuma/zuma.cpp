1b903f48110cf9f57e2de1cb6d7b575410e327e4
MooYongArin <yongariyata@gmail.com>
2024-10-21 09:59:43 +0700

    zuma

---
+++ 02_custom_struct/CP_list/zuma/zuma.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false); 
+    std::cin.tie(0); 
+    int n, k, v;
+    list<int> l;
+    cin >> n >> k >> v;
+    for (size_t i = 0; i < n; i++)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
