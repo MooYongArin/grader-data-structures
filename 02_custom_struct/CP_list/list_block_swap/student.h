ca6f62e8db564596db6c7384fc1e7c5068e7bd3a
MooYongArin <132063700+MooYongArin@users.noreply.github.com>
2024-11-17 10:47:23 +0700

    all_list

---
+++ 02_custom_struct/CP_list/list_block_swap/student.h
@@ -0,0 +1,34 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+#include "list.h"
+
+template <typename T>
+void CP::list<T>::block_swap(iterator a1, iterator a2, iterator b1, iterator b2)
+{
+	// write your code here
+	if (a1 == b2)
+	{
+		std::swap(a1, b1);
+		std::swap(a2, b2);
+	}
+	if (a2 == b1)
+	{
+		node *m = a2.ptr->prev;
+		a1.ptr->prev->next = a2.ptr;
+		a2.ptr->prev = a1.ptr->prev;
+		b2.ptr->prev->next = a1.ptr;
+		a1.ptr->prev = b2.ptr->prev;
+		m->next = b2.ptr;
+		b2.ptr->prev = m;
+	}
+	else
+	{
+		std::swap(a1.ptr->prev->next, b1.ptr->prev->next);
+		std::swap(a1.ptr->prev, b1.ptr->prev);
+		std::swap(a2.ptr->prev->next, b2.ptr->prev->next);
+		std::swap(a2.ptr->prev, b2.ptr->prev);
+	}
+}
+
+#endif
--- 02_custom_struct/CP_list/list_check/student.h
+++ 02_custom_struct/CP_list/list_check/student.h
@@ -2,11 +2,18 @@
 #define __STUDENT_H__
 
 template <typename T>
-bool CP::list<T>::check()
-{
-  // your code here
+bool CP::list<T>::check() {
   node* n = mHeader->next;
-  return true;
+
+  for (size_t i = 0; i < mSize; ++i) {
+
+    if (n == nullptr || n->next == nullptr || n->prev == nullptr || n->next->prev != n || n->prev->next != n) {
+      return false;
+    }
+    n = n->next;
+  }
+
+  return (n == mHeader && mHeader->prev->next == mHeader && mHeader->next->prev == mHeader);
 }
 
 #endif
