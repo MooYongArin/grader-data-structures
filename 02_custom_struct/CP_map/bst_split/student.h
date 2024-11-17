7ef6e8069a406dc69e14e9757b6cf495286a25d7
MooYongArin <132063700+MooYongArin@users.noreply.github.com>
2024-11-17 21:55:07 +0700

    all_map

--- 02_custom_struct/CP_map/bst_split/student.h
+++ 02_custom_struct/CP_map/bst_split/student.h
@@ -10,15 +10,42 @@ CP::map_bst<KeyT,MappedT,CompareT> CP::map_bst<KeyT,MappedT,CompareT>::split(Key
   //your code here
   CP::map_bst<KeyT,MappedT,CompareT> result;
 
-  node* n = mRoot;
-  while (n != nullptr)
-  {
-    if (n->data.first >= val) result.insert(n->data);
-    else erase(n->data.first);
-
-    
+  int currentTree = 0;
+  node* ptr = mRoot;
+  node* parentLeft = NULL;
+  node* parentRight = NULL;
+  while (ptr != NULL) {
+    if (currentTree == 0) {
+      if (compare(val, ptr->data.first) <= 0) {
+        // std::cout << "1 " << ptr->data.first << std::endl;
+        parentLeft = ptr->parent;
+        child_link(parentLeft, val) = NULL;
+        result.child_link(parentRight, val) = ptr;
+        ptr->parent = parentRight;
+        ptr = ptr->left;
+        currentTree = 1;
+      }
+      else {
+        // std::cout << "2 " << ptr->data.first << std::endl;
+        ptr = ptr->right;
+      }
+    }
+    else {
+      if (compare(val, ptr->data.first) > 0) {
+        // std::cout << "3 " << ptr->data.first << std::endl;
+        parentRight = ptr->parent;
+        if (parentRight != NULL) parentRight->left = NULL;
+        child_link(parentLeft, val) = ptr;
+        ptr->parent = parentLeft;
+        ptr = ptr->right;
+        currentTree = 0;
+      }
+      else {
+        // std::cout << "4 " << ptr->data.first << std::endl;
+        ptr = ptr->left;
+      }
+    }
   }
-  
 
   return result;
 }
