fd7dc1bfb94acc9a11ee2205bd583dd9b6d25286
MooYongArin <132063700+MooYongArin@users.noreply.github.com>
2024-11-19 09:00:20 +0700

    k-ary_fix

--- 00_quiz/quiz4/k-ary_path_to_node/path_to_node.cpp
+++ 00_quiz/quiz4/k-ary_path_to_node/path_to_node.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <set>
-#include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -10,18 +8,20 @@ int main(){
     ios_base::sync_with_stdio(false); 
     cin.tie(0); 
     long long n, k, p; 
-    set<long long> s;
+    stack<long long> s;
     cin >> n >> k >> p;
 
     while (p != 0)
     {
-        s.insert(p);
+        s.push(p);
         p = (p-1)/k;
     }
 
     cout << s.size() << endl;
-    for (auto &item : s)
+    while (!s.empty())
     {
-        cout << (item-1)%k << " ";
+        cout << (s.top()-1)%k << " ";
+        s.pop();
     }
+    
 }
