4e54571962530621e266e3ee7e961674d982c49a
MooYongArin <132063700+MooYongArin@users.noreply.github.com>
2024-12-01 22:23:22 +0700

    final_prep

--- test.cpp
+++ test.cpp
@@ -2,9 +2,23 @@
 #include <map>
 
 using namespace std;
+void print_range1(int step, int goal){
+    if (step < goal)
+    {
+        cout << step << " ";
+        print_range1(step+1, goal);
+    }
+}
+void print_range2(int step, int goal){
+    if (step < goal)
+    {
+        print_range2(step+1, goal);
+        cout << step << " ";
+    }
+}
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout << "test";
+    
+    print_range1(0,5);
+     print_range2(0,5);
 }
